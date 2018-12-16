3e2217d6b1b407d95ba7ad7fd4b87137dc315bb4
Lorkaan <lockedandloganed@gmail.com>
2018-12-15 20:55:24 -0500

    Working Hello World test using ReactJS, WebPack and Babel

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Heading extends React.Component{
+
+	render(){
+		return (<div>
+				<Phrase name="Hello World!"/>
+			</div>);
+	}
+}
+
+function Phrase(props){
+	return (<h1>{props.name}</h1>);
+}
+
+
+ReactDOM.render(<Heading/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: './src/HelloWorld.jsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'output.bundle.js'
+	},
+	module:{
+		rules:[
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.htmls$/,
+				use: [
+					{
+						loader: "html-loader"
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html"
+		})
+	]
+};
