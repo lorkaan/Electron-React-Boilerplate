e49c2968e9a00f5fb1edd4a3edab4b94ca41ce3d
Lorkaan <lockedandloganed@gmail.com>
2018-12-17 11:41:55 -0500

    Failed attempt to link electron and react... need a separate build process for each since react sits in the webpage that electron loads

--- src/createElectron.jsx
+++ src/createElectron.jsx
@@ -1,7 +1,7 @@
 const {app, BrowserWindow}= require('electron');
 //import {Heading} from 'HelloWorld.jsx';
 
-/*
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -17,7 +17,7 @@ export class Heading extends React.Component{
 function Phrase(props){
 	return (<h1>{props.name}</h1>);
 }
-*/
+
 
 const main_path = `dist/index.html`;
 
@@ -27,7 +27,6 @@ function createWindow(){
   win = new BrowserWindow({width: 800, height:600});
   win.loadFile(main_path);
   win.webContents.openDevTools();
-  //ReactDOM.render(<Heading/>, document.getElementById('app'));
   win.on('closed', ()=>{
     win = null;
   });
