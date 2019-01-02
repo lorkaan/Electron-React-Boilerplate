a7940cd8b61b61f2e4bbc6a62e3b906da3b8e37b
Laurkan Rodriguez <LRodriguez@quillsoft.ca>
2019-01-02 14:54:16 -0500

    chagned the react.config

--- react.config.js
+++ react.config.js
@@ -10,7 +10,7 @@ module.exports ={
   	module:{
   		rules:[
   			{
-  				test: /react\/\.+\.(js|jsx)$/,
+  				test: /react\/.+\.(js|jsx)$/,
   				exclude: /node_modules/,
   				use: 'babel-loader'
   			},
